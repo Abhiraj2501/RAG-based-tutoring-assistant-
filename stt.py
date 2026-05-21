20f6f1358af7f2ee35dba136903690908b1ca8ef
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-05-22 03:18:17 +0530

    auto: 2026-05-22 03:18:17

--- stt.py
+++ stt.py
@@ -2,4 +2,5 @@ import whisper
 model = whisper.load_model("large-v2")
 
 result = model.transcribe(audio = "audio/1.mp3",
-                          language='')
+                          language='hi',
+                          )
