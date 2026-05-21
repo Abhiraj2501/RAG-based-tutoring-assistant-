f3a304d5ccaaa08d10cd58a2ba219d3cba738442
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-05-22 03:37:13 +0530

    updated stt: translate successful

--- stt.py
+++ stt.py
@@ -1,8 +1,9 @@
 import whisper
+#load the model - large-v2
 model = whisper.load_model("large-v2")
 
 result = model.transcribe(audio = "audios/03_MySQL or Mongo dB.mp3",
                           language='hi',
                           task='translate')
-
+#check the translate
 print(result["text"]) 
